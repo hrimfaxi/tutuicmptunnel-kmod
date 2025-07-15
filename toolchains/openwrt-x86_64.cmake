542426a620b5708b08fb482f30fab8dde1decfe3
hrimfaxi <outmatch@gmail.com>
2025-07-15 10:46:41 +0800

    清理掉openwrt中的加固选项
    这个在交叉编译时的加固选项会造成交叉编译时，由于CMAKE_C_FLAGS_RELEASE
    还没有定义，重设CMAKE_C_FLAGS_RELEASE让它忽略掉-O2等优化选项。
    应该使用add_compile_options而不是设置CMAKE_C_FLAGS_RELEASE。
    
    解决了aarch64和x86_64下CMAKE_BUILD_TYPE=Release时，没有开启任何优化的问题
    
    Signed-off-by: hrimfaxi <outmatch@gmail.com>

--- toolchains/openwrt-x86_64.cmake
+++ toolchains/openwrt-x86_64.cmake
@@ -2,17 +2,6 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} \
--fstrict-flex-arrays=3 \
--fstack-clash-protection -fstack-protector-strong \
--Wl,-z,nodlopen -Wl,-z,noexecstack \
--Wl,-z,relro -Wl,-z,now \
--Wl,--as-needed -Wl,--no-copy-dt-needed-entries \
--Werror -Wtrampolines -Wbidi-chars=any -fPIE -pie -fcf-protection=full -fno-delete-null-pointer-checks -fno-strict-overflow -fno-strict-aliasing -ftrivial-auto-var-init=zero \
--Werror=implicit -Werror=incompatible-pointer-types -Werror=int-conversion \
--Wl,-z,max-page-size=4096 -Wl,--gc-sections \
--fexceptions")
-
 # 指定交叉编译工具链的路径
 set(TOOLCHAIN_PATH $ENV{STAGING_DIR}/toolchain-x86_64_gcc-13.3.0_musl/bin)
 # 设置交叉编译器
