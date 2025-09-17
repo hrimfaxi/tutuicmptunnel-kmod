3758ebeb3a766535f65f22174045f1802ffd0e51
hrimfaxi <outmatch@gmail.com>
2025-09-17 09:21:04 +0800

    移除 packed/aligned 属性以修复 session_map 尺寸与异常会话问题
    代码中多处结构体使用了 attribute((packed)) 与/或 attribute((aligned(16)))，尤其是包含 struct in6_addr 的 key/value。
    同时使用 packed 与 aligned(16) 导致 session_map 的元素尺寸异常，session 表项布局与预期不一致。
    实际表现为：
    - session_map 的大小/对齐异常；
    - 出现大量异常会话记录；
    - 恢复会话偶发性卡顿，需等待一段时间才恢复正常。
    
    移除所有相关结构体上的 packed 与显式 aligned 属性，让编译器采用自然对齐。
    
    Signed-off-by: hrimfaxi <outmatch@gmail.com>

--- common/defs.h
+++ common/defs.h
@@ -35,47 +35,47 @@ enum {
 // 服务器上用于查找udp回包属于哪个客户端
 // 使用tuctl reaper命令定期删除过期项目
 struct session_key {
-  struct in6_addr address __attribute__((aligned(16))); // 客户端地址
-  __be16          sport;                                // 客户端源端口
-  __be16          dport;                                // 客户端目的端口
-} __attribute__((packed));
+  struct in6_addr address; // 客户端地址
+  __be16          sport;   // 客户端源端口
+  __be16          dport;   // 客户端目的端口
+};
 
 struct session_value {
   __u64 age;
   __u8  uid;
-} __attribute__((packed));
+};
 
 // User struct to store in the map
 struct user_info {
-  struct in6_addr address __attribute__((aligned(16))); // IP address in network byte order
-  __be16          icmp_id;                              // ICMP ID
-  __be16          sport;                                // Source port
-  __be16          dport;                                // Destination port
-  __u8            comment[22];                          // Comment
-} __attribute__((packed));
+  struct in6_addr address;     // IP address in network byte order
+  __be16          icmp_id;     // ICMP ID
+  __be16          sport;       // Source port
+  __be16          dport;       // Destination port
+  __u8            comment[22]; // Comment
+};
 
 struct egress_peer_key {
-  struct in6_addr address __attribute__((aligned(16))); // Server address in network byte order
-  __be16          port;                                 // Server port
-} __attribute__((packed));
+  struct in6_addr address; // Server address in network byte order
+  __be16          port;    // Server port
+};
 
 struct egress_peer_value {
   __u8 uid;         // UID for this client
   __u8 comment[22]; // Comment
-} __attribute__((packed));
+};
 
 struct ingress_peer_key {
-  struct in6_addr address __attribute__((aligned(16))); // Server address in network byte order
-  __u8            uid;                                  // UID (icmp id)
-} __attribute__((packed));
+  struct in6_addr address; // Server address in network byte order
+  __u8            uid;     // UID (icmp id)
+};
 
 struct ingress_peer_value {
   __be16 port; // Server UDP Port
-} __attribute__((packed));
+};
 
 // Config struct
 struct config {
-  __u64 packets_processed __attribute__((aligned(8)));
+  __u64 packets_processed;
   __u64 packets_dropped;
   __u64 checksum_errors;
   __u64 fragmented;
@@ -83,10 +83,10 @@ struct config {
   __u32 session_max_age;
   __u8  no_fixup;  // 1 if not using bpf_skb_change_type hack
   __u8  is_server; // 1 if server mode, 0 if client mode
-} __attribute__((packed));
+};
 
 struct gc_switch {
   __u8 enabled;
-} __attribute__((packed));
+};
 
 /* vim: set sw=2 expandtab: */
