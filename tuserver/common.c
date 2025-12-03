c36c2e164422573f768a8ccf32191d8a1efd56fe
hrimfaxi <outmatch@gmail.com>
2025-12-03 12:31:57 +0800

    common.c: 风格改进
    不应该在return后使用else
    
    Signed-off-by: hrimfaxi <outmatch@gmail.com>

--- tuserver/common.c
+++ tuserver/common.c
@@ -306,9 +306,9 @@ size_t scnprintf(char *buf, size_t size, const char *fmt, ...) {
 
   if ((size_t) n >= size) {
     return size - 1;
-  } else {
-    return (size_t) n;
   }
+
+  return (size_t) n;
 }
 
 /* 返回 0 表示成功；若被信号中断，会自动继续睡到足时 */
