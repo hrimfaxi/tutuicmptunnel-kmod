334cb4ddcd60a8d5588b4d2025306de333003fa1
hrimfaxi <outmatch@gmail.com>
2026-06-10 13:21:24 +0800

    common/read_script.h：删除未实现的旧声明，同步实际接口
    Signed-off-by: hrimfaxi <outmatch@gmail.com>

--- common/read_script.h
+++ common/read_script.h
@@ -1,9 +1,10 @@
 #pragma once
 
+#include <stdint.h>
 #include <stdio.h>
 
-int ftello_safe(FILE *stream, off_t *offset);
-int fread_safe(void *ptr, size_t size, size_t nmemb, FILE *stream, size_t *read);
-int read_script(const char *path, char **out, size_t *out_len);
+int64_t file_tell64(FILE *stream);
+int     fread_safe(void *ptr, size_t size, size_t nmemb, FILE *stream, size_t *read);
+int     read_script(const char *path, char **out, size_t *out_len);
 
 // vim: set sw=2 ts=2 expandtab :
